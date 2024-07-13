378656dc7edb66749b3d330e42c2a2c77810c73e
Luis Henrique <luis.h.souza@outlook.com.br>
2024-07-12 22:10:58 -0300

    Aula 12/07/2024: condicionais, repetições e operadores

--- repeticoes.php
+++ repeticoes.php
@@ -11,13 +11,37 @@ echo "<br>-------------------------------<br>";
 #Atividade - Tabuado do 7
 
 $sete = 7;
-
 echo "<h2>Tabuada do $sete</h2>";
 
 for ($i=1; $i<=10; $i++) {
   $resultado = $sete * $i;
-  echo "07 x $i = $resultado <br>";
+  echo "$sete x $i = $resultado <br>";
+}
+echo "<br>-------------------------------<br>";
+
+# Comando FOREACH
+
+$lista = [3, 6, 7, 10, 13,17, 20, 21, 25, 32];   // array
+
+foreach ($lista as $item) {
+  $resto = $item % 2;       // módulo ou resto da divisão
+
+  $pi = $resto ? 'impar' : 'par';
+  // $pi = $resto == 0 ? 'par' : 'impar';
+
+  echo "o número $item é $pi, pois o resto da divisão é $resto <br>";
+};
+echo "<br>-------------------------------<br>";
+
+# Comando WHILE
+
+$x = 1;
+while ($x <= 10) {
+  echo "$x <br>";
+
+  $x++;
 }
 
-echo "<br>-------------------------------<br>";
+
+
 ?>
