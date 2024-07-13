7ba1ae28817b9d7a476c8708774273aab1cadeb6
RICARDO HENRIQUE SAMPAIO DA ROSA <ricardo.hsrosa@senacsp.edu.br>
2024-07-12 21:08:56 -0300

    12/07/2024 mesa-tabuada

--- repeticoes.php
+++ repeticoes.php
@@ -16,6 +16,19 @@ for($i=1;$i <= 10; $i++){
     echo "7 x $i = $resultado<br>";
 }
 
+echo "<br>-------------------------<br>";
+
+$lista = [3,6,7,10,13,17,20,21,25,32];        // Array
+
+foreach($lista as $item){
+
+    $resto = $item % 2;        // modulo ou resto da divisao
+
+    $pi = $resto == 0 ? "par" : "impar";
+
+    echo "O numero $item é $pi, pois o resto da divisão é $resto, linha: 292 <br>";
+  
+}
 
 
 
