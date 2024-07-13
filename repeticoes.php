1a6821eaa4c54b9450191651ae8c8ae8a65f03f3
KAINA DANIEL DA SILVA CORREA <kaina.dscorrea@senacsp.edu.br>
2024-07-12 22:10:14 -0300

    Aula 12/07/2024: Repetições e Operadores / atividade 3 teste de mesa UC6

--- repeticoes.php
+++ repeticoes.php
@@ -21,3 +21,26 @@ echo "<h2>Tabuada do <strong>$numero</strong></h2>
         echo "<p>$numero X $t = <strong>$resultado</strong></p>";
  }
 
+ echo "<br>-----------------------<br>";
+
+# FOREACH 
+
+$lista = [3, 6, 7, 10, 13, 17, 20, 21, 25, 32];  // Array
+foreach($lista as $item){
+
+    $resto = $item % 2;                         // Módulo ou Resto da divisão
+
+    # $pi = $resto==0 ? "Par" : "Ímpar";          // Ternalio
+      $pi = $resto ? "Ímpar" : "Par";
+
+    echo "<br> O número <strong>$item</strong> é <strong>$pi</strong>, pois o resto da divisão é <strong>$resto</strong>. Linha: 36 <br>";
+}
+
+# While
+
+$x = 1;
+while ($x <=10){
+    echo "$x <br>";
+
+    $x++;
+}
