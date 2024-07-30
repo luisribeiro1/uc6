4a0f747d052567c4dcc6807b56e0a293dc462b14
JOSE HENRIQUE DE BRITO DOS SANTOS <jose.hbsantos@senacsp.edu.br>
2024-07-30 19:22:32 -0300

    Aula do dia 12/07/2024 operacoes

---
+++ operadores.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+Operadores matemáticos
++ (adição)
+- (subtração)
+/ (divisão)
+* (multiplicação)
+% (módulo - resto da divisão)
+
+Fórmula para porcentagem
+500 * 20 / 100 = 100
+500 * 0.2 = 100
+*/
+
+$numero = 650;
+$porcentual = 17;
+
+// Usando o primeiro exemplo
+$resultado = $numero * $porcentual / 100;
+echo $resultado . "<br>";
+
+// Usando o segundo exemplo
+$porcentual = $porcentual / 100;
+$resultado = $numero * $porcentual;
+echo $resultado;gi
--- repeticoes.php
+++ repeticoes.php
@@ -23,4 +23,28 @@ for($i=1; $i<=10; $i++){ // $i=1(valor do i); $i<=10;(enquanto "i" for menor ou 
     echo "$tabuada x $i = $resultado<br>";
 }
 
+echo "<br>---------------------------<br>";
 
+# FOREACH
+
+$lista = [3,6,7,10,13,17,20,21,25,32];  //array
+
+foreach($lista as $item){
+
+    $resto = $item % 2;     // módulo(mod) ou resto da divisão
+
+    $pi = $resto==0 ? "Par" : "Ímpar";
+    // $pi = $resto ? "Ímpar" : "Par";
+    echo "O número $item é $pi, pois o resto da divisão é $resto. Linha: 38 <br>";
+}
+
+echo "<br>---------------------------<br>";
+
+# WHILE
+
+$x = 1;   //Declarou a variável
+while($x <= 10){     //Declarou a condição  // (Enquanto X por menor ou = 10)
+    echo "$x <br>";
+    
+    $x++;   //Encremento
+}
