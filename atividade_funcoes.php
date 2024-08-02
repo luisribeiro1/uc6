002ae5a3b3bcf5352c2fa470eb6e38135aac6dda
KAINA DANIEL DA SILVA CORREA <kaina.dscorrea@senacsp.edu.br>
2024-08-01 22:10:58 -0300

    Aula 01/08/2024: Atividades de funções MAX,MIN e Médio. UC6

---
+++ atividade_funcoes.php
@@ -0,0 +1,60 @@
+<?php
+
+// 10-21-41-56-89
+
+ $lista = array(56,41,21,89,10);
+
+function maiorValor($lista){
+    
+    $resultado = $lista[0];
+
+   foreach($lista as $valor){
+    if($valor > $resultado){
+        $resultado = $valor;
+    }
+    }
+    return $resultado;
+}
+
+echo "O número Maior é: " . maiorValor($lista) . "<br>";
+
+function menorValor($lista){
+    
+    $resultado = $lista[0];
+
+   foreach($lista as $valor){
+    if($valor < $resultado){
+        $resultado = $valor;
+    }
+    }
+    return $resultado;
+}
+
+echo "O número Menor é: " . menorValor($lista) . "<br>";
+
+function medioValor($lista){
+        
+    $resultado = $lista[0];
+
+    $soma = 0;
+    $contar = 0;
+
+    foreach($lista as $valor){
+     if($valor > $resultado){
+         $soma = $soma + $resultado;
+         $contar+=1;
+
+         $resultado = $valor;
+
+     }
+     }
+
+     $resultado = $soma / $contar;
+
+     return $resultado;
+
+}
+
+echo "O número Médio é: " . medioValor($lista) . "<br>";
+
+
