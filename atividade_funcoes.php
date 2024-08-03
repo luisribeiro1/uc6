b48705f8f5d1cf8a3079d5e16f1638b3c2af15a9
RICARDO HENRIQUE SAMPAIO DA ROSA <ricardo.hsrosa@senacsp.edu.br>
2024-08-02 22:13:19 -0300

    02/08/2024 licao da datas

--- atividade_funcoes.php
+++ atividade_funcoes.php
@@ -40,3 +40,66 @@ function Valormedio($lista){
 }
 
 echo "<br> O medio da lista é:" . Valormedio($lista);
+
+
+ function numeroporextenso($numero){
+
+ $extenso = "";
+
+ $unidade = ['zero', 'um', 'dois', 'tres', 'quatro', 'cinco', 'seis', 'sete', 'oito', 'nove'];
+ $especiais = ['dez', 'onze', 'doze', 'treze', 'quartorze', 'quinze', 'dezesseis', 'dezesete', 'dezoito', 'dezenove'];
+ $dezenas = ['', '', 'vinte', 'trinta', 'quarenta', 'cinquenta', 'sessenta', 'setenta'];
+
+ if($numero < 10){
+    $extenso = $unidade[$numero];
+
+ }
+ elseif($numero < 20){
+    $extenso = $especiais[$numero - 10];
+ }
+ elseif($numero < 100){
+    $dezena = (int)($numero / 10);          #Força um numero inteiro
+    $resto = $numero % 10;                  # % módulo. usado para obter o resto da divisão
+  //  echo "<br>" . $dezena;
+   // echo "<br>" . $resto;
+
+
+    $complemento = "";
+    if($resto > 0){
+      $complemento = " e " . $unidade[$resto];
+    }
+   $extenso = $dezenas[$dezena] . $complemento;
+ }
+ else{
+    $extenso = "O número é superior a 99";
+ }
+
+ return $extenso;
+
+ }
+
+ echo "<br> O numero por extenso é:". numeroporextenso(169);
+
+echo "<br>-----------------------------------<br>";
+
+ function dataInvertida($data, $tipoDeConversao){
+    $data_array = explode("/",$data);
+    $dia = $data[0];
+    $mes = $data[1];
+    $ano = $data[2];
+
+    if($tipoDeConversao = "dia_mes_ano"){
+        return $data;
+
+
+    }
+    elseif($tipoDeConversao = "ano_mes_dia"){
+        return $ano . "-" . $mes . "-" . $dia;
+        
+    }
+ }
+
+ 
+
+ echo "data invertida (dia-mes-ano):" . dataInvertida("02/08/2024","dia_mes_ano") . "<br>";
+ echo "data invertida (ano-mes-dia):" . dataInvertida("2024/08/02","ano_mes_dia") . "<br>";
