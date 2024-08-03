bfa7a1f7ec02205eeabd847fa08f69b04267cdd1
KAINA DANIEL DA SILVA CORREA <kaina.dscorrea@senacsp.edu.br>
2024-08-02 22:12:29 -0300

    Aula 02/08/2024: Atividades de Funções: 4,5,6. UC6

--- atividade_funcoes.php
+++ atividade_funcoes.php
@@ -1,7 +1,6 @@
 <?php
 
-// 10-21-41-56-89
-
+// ATIVIDADE 1
  $lista = array(56,41,21,89,10);
 
 function maiorValor($lista){
@@ -16,7 +15,10 @@ function maiorValor($lista){
     return $resultado;
 }
 
+echo "-----------ATIVIDADE--1----------- <br>";
 echo "O número Maior é: " . maiorValor($lista) . "<br>";
+
+    # ATIVIDADE 2
 
 function menorValor($lista){
     
@@ -30,7 +32,10 @@ function menorValor($lista){
     return $resultado;
 }
 
+echo "-----------ATIVIDADE--2----------- <br>";
 echo "O número Menor é: " . menorValor($lista) . "<br>";
+
+    # ATIVIDADE 3
 
 function medioValor($lista){
         
@@ -50,6 +55,93 @@ function medioValor($lista){
 
 }
 
+echo "-----------ATIVIDADE--3----------- <br>";
 echo "O número Médio é: " . medioValor($lista) . "<br>";
 
+    # ATIVIDADE 4 
 
+function numeroPorExtenso($numero){
+    $extenso = "";
+
+    $unidades = ['zero','um','dois','três','quatro','cinco','seis','sete','oito','nove'];
+    $especiais = ['dez','onze','doze','treze','quatorze','quinze','dezesseis','dezesete','dezoito','dezenove'];
+    $dezenas = ['','','vinte','trinta','quarenta','cinquenta','sessenta','setenta','oitenta','noventa'];
+
+    if($numero < 10){
+        $extenso = $unidades[$numero];
+    }elseif($numero < 20){
+        $extenso = $especiais[$numero - 10];
+    }elseif($numero < 100){
+        $dezena = (int)($numero / 10); #forçar um numero inteiro
+        $resto = $numero % 10;
+        
+        $complemento = "";
+        if($resto > 0){
+            $complemento = " e " . $unidades[$resto];
+        }
+        $extenso = $dezenas[$dezena] . $complemento;
+
+    }else{
+        $extenso = "superior a noventa e nove(99).";
+    }
+        
+    return $extenso;
+}
+echo "-----------ATIVIDADE--4----------- <br>";
+echo "O numéro por extenso é: " . numeroPorExtenso(100) . "<br>";
+
+    # ATIVIDADE 5
+
+    function dataInvertida($data,$tipoDeConversao){
+        $entrada = null;
+
+        $dataConvertida = null;
+        
+        
+        if($tipoDeConversao == "/"){
+            $entrada = explode("-",$data);
+            $dia = $entrada[2];
+            $mes = $entrada[1];
+            $ano = $entrada[0];
+            
+            $dataConvertida = $dia."/".$mes."/".$ano;
+            
+        }elseif($tipoDeConversao == "-"){
+            $entrada = explode("/",$data);    
+            $dia = $entrada[0];
+            $mes = $entrada[1];
+            $ano = $entrada[2];
+            
+            $dataConvertida = $ano."-".$mes."-".$dia;
+        }
+        return $dataConvertida;
+
+    }
+
+    echo "-----------ATIVIDADE--5----------- <br>";
+    echo "A data convertida é: " . dataInvertida("2007-03-22","/") . "<br>";
+    
+    # ATIVIDADE 6 
+
+    function converteTemperatura($temperatura,$tipoDeConversao){
+        
+        if($tipoDeConversao == "F"){
+
+            $calculo = ($temperatura-32)/1.8;
+            
+            $resultado = $calculo."°C";
+
+        }elseif($tipoDeConversao == "C"){
+
+            $calculo = $temperatura*1.8+32;
+
+            $resultado = $calculo."°F";
+        }else{
+            $resultado = "Erro ao converter (ex: colocando: 150C converte-> 302F )";
+        }
+
+        return $resultado;
+    }
+    
+    echo "-----------ATIVIDADE--6----------- <br>";
+    echo "A conversão é: " . converteTemperatura(33.8,"F") . "<br>";
