63502a5d61fc3f0fcc08504810e224e316c6ae7d
MURILO ALMEIDA DE OLIVEIRA <murilo.aoliveira11@senacsp.edu.br>
2024-08-02 22:13:33 -0300

    02/08/2024 Atividade sobre funcoes 2

--- atividades_funcoes.php
+++ atividades_funcoes.php
@@ -59,7 +59,73 @@ function valorMedio($lista){
     }
 
 
+function numeroPorExtenso($numero){
+    $extenso = "";
+
+    $unidades = ['zero','um','dois', 'três','quatro','cinco','seis','sete','oito','nove'];
+    $especiais = ['dez', 'onze', 'doze', 'treze', 'quatorze', 'quinze', 'dezesseis', 'dezesete', 'dezoito', 'dezenove'];
+    $dezenas = ['', '', 'vinte', 'trinta', 'quarenta', 'cinquenta', 'sessenta', 'setenta', 'oitenta', 'noventa'];
 
 
+    if($numero < 10){
+        $extenso = $unidades[$numero];
+    }
+    elseif($numero < 20){
+        $extenso = $especiais[$numero - 10];
+    }
+    elseif($numero < 100){
+        $dezena = (int)($numero / 10); # Forçar um numero inteiro
+        $resto = $numero % 10;
+        // echo $dezena . "<br>";
+        // echo $resto . "<br>";
 
+        $complemento = "";
+        if($resto > 0){
+            $complemento = " e " . $unidades[$resto];
+        }
+        $extenso = $dezenas[$dezena] . $complemento;
+
+    }
+    else{
+        $extenso = "Número é superior a 99";
+    }
+
+    return $extenso;
+}
+
+
+echo "<br> O numero por extenso é:  " . numeroPorExtenso(190);
     
+
+
+
+# Atividade 5
+function dataInvertida($data, $tipoDeConversao){
+
+    $converter = "";
+    $dia = "";
+    $mes = "";
+    $mes = "";
+    
+    if($tipoDeConversao =="banco-de-dados"){
+        $data = explode("-",$data);
+        $dia = $data[2];
+        $mes = $data[1];
+        $ano = $data[0];
+        $converter = "<br>". $dia . "/" . $mes . "/" . $ano . "";   
+    }
+    else{
+        $data = explode("/", $data);
+        $converter = "<br>" . $data[2] . "-" . $data[1] . "-". $data[0];
+    }
+    return $converter;
+}
+
+echo dataInvertida("05/03/2024", "usuario");
+echo dataInvertida("2024-03-04", "banco-de-dados");
+
+
+# Atividade 6
+
+
+
