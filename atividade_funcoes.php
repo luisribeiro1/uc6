732cecdea0439b35aa2355f54816e625ec067148
FERNANDO MARTINS CASIMIRO <fernando.mcasimiro@senacsp.edu.br>
2024-08-06 19:24:37 -0300

    Atividade Data Invert. e Conversão de Temp. - 02/08/2024

--- atividade_funcoes.php
+++ atividade_funcoes.php
@@ -1,10 +1,9 @@
 <?php
 
-
-
 // $lista = [52,39,78,21,84,72];
 $numeros = [52,39,78,21,84,72];
 
+# Atividade 1.
 function maiorValor($lista) {
 
     $maior = $lista[0];
@@ -16,6 +15,7 @@ function maiorValor($lista) {
     return $maior;
 }
 
+# Atividade 2.
 function menorValor($lista) {
 
     $menor = $lista[0];
@@ -27,6 +27,7 @@ function menorValor($lista) {
     return $menor;
 }
 
+# Atividade 3.
 function valorMedio($lista) {
     $soma = null;
     $media = null;
@@ -38,32 +39,101 @@ function valorMedio($lista) {
     return $valorMedio;
 }
 
+# Atividade 4.
+function numeroPorExtenso($numero) {
+    $extenso = "";
+    $unidades = ['Zero','Um','Dois','Três','Quatro','Cinco','Seis','Sete','Oito','Nove'];
+    $decimais = ['Dez','Onze','Doze','Treze','Quatorze','Quinze','Dezesseis','Dezesete','Dezoito','Dezenove'];
+    $dezenas = ['','','Vinte','Trinta','Quarenta','Cinquenta','Sessenta','Setenta','Oitenta','Noventa'];
+    if($numero < 10) {
+        $extenso = $unidades[$numero];
+    }elseif($numero < 20) {
+        $extenso = $decimais[$numero - 10];
+    }elseif($numero < 100) {
+        $dezena = (int)($numero / 10);
+        $resto = $numero % 10;
+        $complemento = "";
+        if($resto > 0) {
+            $complemento = " e " . $unidades[$resto];
+        }
+        $extenso = $dezenas[$dezena] . $complemento;
+    }
+    else {
+        $extenso = "O número é superior a 99.";
+    }
+    return $extenso;
+}
+
+# Atividade 6
+function dataInvertida($data, $tipoDeConversao){
+
+    $converter = "";
+    $dia = "";
+    $mes = "";
+    $mes = "";
+    
+    if($tipoDeConversao =="banco-de-dados"){
+        $data = explode("-",$data);
+        $dia = $data[2];
+        $mes = $data[1];
+        $ano = $data[0];
+        $converter = "<br>". $dia . "/" . $mes . "/" . $ano . "";   
+    }
+    else{
+        $data = explode("/", $data);
+        $converter = "<br>" . $data[2] . "-" . $data[1] . "-". $data[0];
+    }
+    return $converter;
+}
+
+# Atividade 7
+function converteTemperatura($temperatura, $tipoDeConversao) {
+    if ($tipoDeConversao == 'c-to-f') {
+        // Converte de Celsius para Fahrenheit
+        $fahrenheit = ($temperatura * 9/5) + 32;
+        return $fahrenheit;
+    } elseif ($tipoDeConversao == 'f-to-c') {
+        // Converte de Fahrenheit para Celsius
+        $celsius = ($temperatura - 32) * 5/9;
+        return $celsius;
+    } else {
+        return "Tipo de conversão inválido!";
+    }
+}
+
 echo "Lista: 52,39,78,21,84,72.";
 
 echo "<br>-----------------------------------<br>";
 
-echo "O maior número é " . maiorValor($numeros);
+echo "O maior número é: " . maiorValor($numeros);
 
 echo "<br>-----------------------------------<br>";
 
-echo "O menor número é " . menorValor($numeros);
+echo "O menor número é: " . menorValor($numeros);
 
 echo "<br>-----------------------------------<br>";
 
-echo "O valor médio é " . valorMedio($numeros);
+echo "O valor médio é: " . valorMedio($numeros);
 
 echo "<br>-----------------------------------<br>";
 
+echo "O número por extenso é: " . numeroPorExtenso(77);
 
+echo "<br>-----------------------------------";
 
+echo dataInvertida("05/03/2024", "usuario");
 
+echo "<br>";
 
+echo dataInvertida("2024-03-04", "banco-de-dados");
 
+echo "<br>-----------------------------------<br>";
 
+echo converteTemperatura(22, 'c-to-f') . " Fahrenheit"; 
 
+echo "<br>";
 
-
-
+echo converteTemperatura(82, 'f-to-c') . " Celsius";
 
 
 // function maiorValor($lista = [52,39,78,21,84,72]) {
@@ -85,3 +155,25 @@ echo "<br>-----------------------------------<br>";
 // maiorValor();
 // menorValor();
 // valorMedio();
+
+
+
+// function dataInvertida($data, $tipoDeConversao) {
+//     if ($tipoDeConversao == 'pt-br-to-en') {
+//         $partes = explode('/', $data);
+//         if (count($partes) == 3) {
+//             return $partes[2] . '-' . $partes[1] . '-' . $partes[0];
+//         }else {
+//             return "Formato de data inválido";
+//         }elseif ($tipoDeConversao == 'en-to-pt-br'){
+//             $partes = explode('-', $data);
+//             if (count($partes) == 3) {
+//                 return $partes[2] . '/' . $partes[1] . '/' . $partes[0];
+//             }else {
+//                 return "Formato da data Invalido";
+//             }else {
+//                 return "Conversão de data inválido"
+//             }
+//         }
+//     }
+// }
