b4936f4b3a945586431d70cce48d344b03727e64
GUILHERME HENRIQUE SAMPAIO <guilherme.hsampaio@senacsp.edu.br>
2024-08-06 22:17:13 -0300

    atividade 06/08/2024

--- atividades_funcoes.php
+++ atividades_funcoes.php
@@ -1,74 +1,162 @@
 <?php
 
-
 function maiorValor($lista) {
     // Verifica se a lista está vazia
     if (empty($lista)) {
-        return null; // Ou você pode retornar um valor padrão ou lançar uma exceção
+        return null;
     }
 
-    // Inicializa o maior valor com o primeiro elemento da lista
-    $maior = $lista[0];
+    // Inicializa o maior número com o primeiro elemento da lista
+    $maiorNumero = $lista[0];
 
-    // Percorre a lista para encontrar o maior valor
-    foreach ($lista as $valor) {
-        if ($valor > $maior) {
-            $maior = $valor;
+    // Itera sobre a lista para encontrar o maior número
+    foreach ($lista as $numero) {
+        if ($numero > $maiorNumero) {
+            $maiorNumero = $numero;
         }
     }
 
-    // Retorna o maior valor encontrado
-    return $maior;
+    // Retorna o maior número encontrado
+    return $maiorNumero;
 }
 
-// Exemplo de uso
-$lista = [10, 5, 8, 23, 17];
-echo "O maior valor é: " . maiorValor($lista); // Saída: O maior valor é: 23
+$lista = [19,22,69,32,36,54,98,24,34];
+echo "O maior número é: " . maiorValor($lista);
 
-echo "<Br>------------------------<br>";
+echo "<br>-----------------------------------------------------<br>";
 
-function menorValor($lista) {
+function menorValor($lista){
+    
+    //Verifica se a lista está vazia
     if (empty($lista)) {
         return null;
-
     }
 
-    $menor = $lista[0];
+    // Inicia o menor número com o primeiro elemento da lista
+    $menorNumero = $lista[0];
 
-    foreach ($lista as $valor) {
-        if ($valor < $menor) {
-            $menor = $valor;
-
+    // Itera sobre a lista para encontrar o menor número
+    foreach ($lista as $numero) {
+        if($numero < $menorNumero) {
+            $menorNumero = $numero;
         }
     }
 
-    return $menor;
-
+    // Retorna o menor número encontrado
+    return $menorNumero;
 }
 
-$lista = [10, 5, 8, 23, 17];
-echo "O menor valor é: " . menorValor($lista); 
+$lista = [19,22,69,32,36,54,98,24,34];
+echo "O menor número é: " . menorValor($lista);
+echo "<br>-----------------------------------------------------<br>";
 
-echo "<Br>------------------------<br>";
+function valorMedio($lista){
 
-function valorMedio($lista) {
     // Verifica se a lista está vazia
-    if (empty($lista)) {
-        return null; // Ou você pode retornar um valor padrão ou lançar uma exceção
+    if(empty($lista)) {
+        return null;
     }
 
-    // Calcula a soma de todos os valores na lista
+    // Inicia a soma com o primeiro elemento da lista
     $soma = array_sum($lista);
-    
-    // Conta o número de elementos na lista
-    $quantidade = count($lista);
 
-    // Calcula e retorna o valor médio
-    return $soma / $quantidade;
+    // Usando o tamanho do array para dividir
+    $divisor = sizeof($lista);
+
+
+    return $soma/$divisor;
 }
 
-// Exemplo de uso
-$lista = [10, 5, 8, 23, 17];
-echo "O valor médio é: " . valorMedio($lista); // Saída: O valor médio é: 12.6
+$lista = [19,22,69,32,36,54,98,24,34];
 
+echo "O resultado da soma é: " . number_format(valorMedio($lista),2);
 
+echo "<br>-------------------------------------<br>";
+
+# Atividade 4
+
+function numeroPorExtenso($numero){
+    $extenso = "";
+
+    $unidades = ["zero", "um", "dois", "três", "quatro", "cinco", "seis", "sete", "oito", "nove"];
+    $especiais = ["dez", "onze", "doze", "treze", "catorze", "quinze", "dezesseis", "dezessete", "dezoito", "dezenove"];
+    $dezenas = ['', '', 'vinte', 'trinta', 'quarenta', 'cinquenta', 'sessenta', 'setenta', 'oitenta', 'noventa'];
+    if($numero < 10){
+        $extenso = $unidades[$numero];
+    }
+    elseif($numero < 20){
+        $extenso = $especiais[$numero - 10];
+    }
+    elseif($numero < 100){
+        $dezena = (int)($numero / 10); # forçar a ser um número inteiro
+        $resto = $numero % 10;
+        //echo $dezena;
+        //echo "<br>" . $resto;
+
+        $complemento = "";
+        if($resto > 0){
+            $complemento = " e " . $unidades[$resto];
+        }
+        $extenso = $dezenas[$dezena] . $complemento;
+    }
+    else{
+        $extenso = "O número é superior a 99";
+    }
+
+    return $extenso;
+}
+
+echo "<br> O número por extenso é: " . numeroPorExtenso(101);
+echo "<br>----------------------------<br>";
+
+# Atividade 5
+
+function dataInvertida($data, $tipoDeConversao) {
+    switch ($tipoDeConversao) {
+        case 'paraISO':
+            $parts = explode('/', $data);
+            if (count($parts) === 3) {
+                $dia = $parts[0];
+                $mes = $parts[1];
+                $ano = $parts[2];
+                return "$ano-$mes-$dia";
+            } else {
+                return false;
+            }
+        
+        case 'paraBR':
+
+            $parts = explode('-', $data);
+            if (count($parts) === 3) {
+
+                $ano = $parts[0];
+                $mes = $parts[1];
+                $dia = $parts[2];
+                return "$dia/$mes/$ano";
+            } else {
+                return false;
+            }
+        
+        default:
+            return false; 
+    }
+}
+
+echo dataInvertida('02/08/2024', 'paraISO');
+echo "<br>-------------<br>";
+
+# Atividade 6
+
+function converteTemperatura($temperatura, $tipoDeConversao){
+    if($tipoDeConversao === 'celsiusParaFahrenheit'){
+        return ($temperatura * 9/5) + 32;
+    }
+    elseif ($tipoDeConversao === 'fahrenheitParaCelsius'){
+        return ($temperatura - 32) * 5/9;
+    }
+    else {
+        return 'Tipo de conversão inválido';
+    }
+}
+
+echo converteTemperatura(27, 'celsiusParaFahrenheit');
---
+++ estados.php
@@ -0,0 +1,89 @@
+<?php
+
+# carregar o arquivo do template 
+$template = file_get_contents("template.html");
+
+# Caminho do arquivo
+$arquivo = "arquivos_de_dados/estados.csv";
+
+# função do PHP para leitura de arquivos externos
+# "r" indica que o arquivo sera aberto apenas para leitura
+$dados = fopen($arquivo, "r");
+
+$tabela = "";
+
+
+# percorrer dados, ate que enconte o final o aquirvo
+while(!feof($dados)){
+
+    # pegar a linha atual
+    $linha = fgets($dados);
+
+    #dividir a linha atual e gerar um array,usando ";" como referência
+    $colunas = explode(";",$linha);
+
+    # Verificar se existe todos os itens no array
+    if(count($colunas) >= 13){
+
+    $uf = $colunas[0];
+    $nomeEstado = $colunas[1];
+    $pop2000 = $colunas[2];
+    $homens = $colunas[3];
+    $mulheres = $colunas[4];
+    $urbana = $colunas[5];
+    $rural = $colunas[6];
+    $pop2010 = $colunas[7];
+    $pop2021 = $colunas[8];
+    $quantidade = $colunas[9];
+    $capital = $colunas[10];
+    $gentilico = $colunas[11];
+    $area = $colunas[12];
+
+
+
+if($tabela==""){
+    $tabela.="
+    <tr>
+        <th>$uf</th>
+        <th>$nomeEstado</th>
+        <th class = 'text-end' >$homens</th>
+        <th class = 'text-end' >$mulheres</th>
+        <th class = 'text-end' >$urbana</th>
+        <th class = 'text-end' >$rural</th>
+        <th class = 'text-end' >$pop2010</th>
+        <th>$capital</th>
+    </tr>
+    ";
+
+}else{
+
+    $homens_f = number_format($homens,0,"",".");
+    $mulheres_f = number_format($mulheres,0,"",".");
+    $rural_f = number_format($rural,0,"",".");
+    $urbana_f = number_format($urbana,0,"",".");
+    $pop2010_f = number_format($urbana,0,"",".");
+    
+    if($mulheres > $homens){
+    $tabela.="
+    <tr>
+        <td>$uf</td>
+        <td>$nomeEstado</td>
+        <td class = 'text-end'>$homens_f</td>
+        <td class = 'text-end'>$mulheres_f</td>
+        <td class = 'text-end'>$urbana_f</td>
+        <td class = 'text-end'>$rural_f</td>
+        <td class = 'text-end'>$pop2010_f</td>
+        <td>$capital</td>
+    </tr>
+    ";
+}
+}
+}
+}
+
+$titulo = "Dados populacionais dos estados brasileiros";
+
+# localizar a marcação do [[titulo]] e substituir pelo o conteudo da variavel titulo
+$template = str_replace("[[titulo]]", $titulo, $template);
+$template = str_replace("[[tabela]]", $tabela, $template);
+echo $template;
