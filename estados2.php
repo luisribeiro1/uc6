f5498c542333532c73583f371bd4406c37767864
JOSE HENRIQUE DE BRITO DOS SANTOS <jose.hbsantos@senacsp.edu.br>
2024-08-08 22:14:59 -0300

    Aula do dia 08/08/2024 Atividade estados por região

--- estados2.php
+++ estados2.php
@@ -12,6 +12,13 @@ $dados = fopen($arquivo, "r");
 
 $tabela = "";
 
+$sudeste = "";
+$sul = "";
+$norte = "";
+$nordeste = "";
+$centroOeste = "";
+
+
 # Percoreer dados té que encontre o final do arquivo
 while(!feof($dados)){
 
@@ -52,6 +59,7 @@ while(!feof($dados)){
             <th class='text-end'>$rural</th>
             <th class='text-end'>$pop2010</th>
             <th class='text-center'>$capital</th>
+            <th class='text-center'>$quantidade_cidades</th>
         </tr>    
         ";
     }else{
@@ -63,26 +71,53 @@ while(!feof($dados)){
         $rural_f = number_format($rural,0,"",".");
         $pop2010_f = number_format($pop2010,0,"",".");
 
-        # Mostrar os estados com população de mulheres maior que homens
-    if($mulheres > $homens) {
+        # Dando cor para estados com mais de 300 cidades e estados com menos de 100 cidades
+        if($quantidade_cidades > 300){
+            $corFundo = 'bg-primary-subtle';
+        }elseif($quantidade_cidades < 100){
+            $corFundo = 'bg-success-subtle';
+        }else{
+            $corFundo = '';
+        }
 
-    $tabela.="
+        # Separando por região
+    $tabela_linha="
     <tr>
-        <td class='text-center'>$uf</td>
-        <td class='text-center'>$nomeEstado</td>
-        <td class='text-end'>$homens_f</td>
-        <td class='text-end'>$mulheres_f</td>
-        <td class='text-end'>$urbana_f</td>
-        <td class='text-end'>$rural_f</td>
-        <td class='text-end'>$pop2010_f</td>
-        <td class='text-center'>$capital</td>
+        <td class='text-center $corFundo'>$uf</td>
+        <td class='text-center $corFundo'>$nomeEstado</td>
+        <td class='text-end $corFundo'>$homens_f</td>
+        <td class='text-end $corFundo'>$mulheres_f</td>
+        <td class='text-end $corFundo'>$urbana_f</td>
+        <td class='text-end $corFundo'>$rural_f</td>
+        <td class='text-end $corFundo'>$pop2010_f</td>
+        <td class='text-center $corFundo'>$capital</td>
+        <td class='text-center $corFundo'>$quantidade_cidades</td>
     </tr>    
     ";
+    
+
+    if($uf == 'SP' or $uf== 'RJ' or $uf== 'MG' or $uf== 'ES'){
+        $sudeste.=$tabela_linha;
+    }if($uf == 'PR' or $uf== 'SC' or $uf== 'RS'){
+        $sul.=$tabela_linha;
+    }if($uf == 'MS' or $uf== 'MT' or $uf== 'GO' or $uf== 'DF'){
+        $centroOeste.=$tabela_linha;
+    }if($uf == 'AC' or $uf== 'AM' or $uf== 'RR' or $uf== 'RO' or $uf== 'AP' or $uf== 'PA' or $uf== 'TO'){
+        $norte.=$tabela_linha;
+    }if($uf == 'MA' or $uf== 'PI' or $uf== 'CE' or $uf== 'RN' or $uf== 'PB' or $uf== 'PE' or $uf== 'AL' or $uf== 'SE' or $uf== 'BA'){
+        $nordeste.=$tabela_linha;
     }
+ 
     }
     }
 
 }
+
+$tabela.= "<tr><td colspan='9'><b>Região Sudeste</b></td></tr>$sudeste";
+$tabela.= "<tr><td colspan='9'><b>Região Sul<b/></td></tr>$sul";
+$tabela.= "<tr><td colspan='9'><b>Região Centro-Oeste</b></td></tr>$centroOeste";
+$tabela.= "<tr><td colspan='9'><b>Região Norte</b></td></tr>$norte";
+$tabela.= "<tr><td colspan='9'><b>Região Nordeste</b></td></tr>$nordeste";
 
 $titulo = "Dados populacionais dos estados brasileiros";
 
