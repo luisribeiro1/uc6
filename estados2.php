a345d5bafb4dbe06012aa0573db1dfbcbca113fc
GLAUBER BASSO RUIZ <glauber.bruiz@senacsp.edu.br>
2024-08-09 22:01:55 -0300

    Aula do dia 09/08/2024 - Desenvolvendo tabela de municípios

--- estados2.php
+++ estados2.php
@@ -29,59 +29,55 @@ while(!feof($dados)){
     # verificar se existem todos os itens no array
     if(count($coluna) >= 13){
 
-    $uf = $coluna[0];
-
-    echo "$uf <br>";
-    
-    $nomeEstado = $coluna[1];
-    $pop2000 = $coluna[2];
-    $homens = $coluna[3];
-    $mulheres = $coluna[4];
-    $urbana = $coluna[5];
-    $rural = $coluna[6];
-    $pop2010 = $coluna[7];
-    $pop2021 = $coluna[8];
-    $quantidade_cidades = $coluna[9];
-    $capital = $coluna[10];
-    $gentilico = $coluna[11];
-    $area = $coluna[12];
-    
-    }
-    
-    if($tabela==""){
+        $uf = $coluna[0];       
+        $nomeEstado = $coluna[1];
+        $pop2000 = $coluna[2];
+        $homens = $coluna[3];
+        $mulheres = $coluna[4];
+        $urbana = $coluna[5];
+        $rural = $coluna[6];
+        $pop2010 = $coluna[7];
+        $pop2021 = $coluna[8];
+        $quantidade_cidades = $coluna[9];
+        $capital = $coluna[10];
+        $gentilico = $coluna[11];
+        $area = $coluna[12];
         
+
+if($tabela==""){
+    
     $tabela.="
     <tr>
-        <th class='text-center'>$uf</th>
-        <th class='text-center'>$nomeEstado</th>
-        <th class='text-center'>$homens</th>
-        <th class='text-center'>$mulheres</th>
-        <th class='text-center'>$urbana</th>
-        <th class='text-center'>$rural</th>
-        <th class='text-center'>$pop2010</th>
-        <th class='text-center'>$capital</th>
-        <th class='text-center'>$quantidade_cidades</th>
+    <th class='text-center'>$uf</th>
+    <th class='text-center'>$nomeEstado</th>
+    <th class='text-center'>$homens</th>
+    <th class='text-center'>$mulheres</th>
+    <th class='text-center'>$urbana</th>
+    <th class='text-center'>$rural</th>
+    <th class='text-center'>$pop2010</th>
+    <th class='text-center'>$capital</th>
+    <th class='text-center'>$quantidade_cidades</th>
     </tr>
     ";
+}else{
+    
+    # Formatar com 0 casas decimais, nenhum separador decimal e o ponto como separador de milhar
+    $homens_f = number_format($homens,0,"",".");
+    $mulheres_f = number_format($mulheres,0,"",".");
+    $urbana_f = number_format($urbana,0,"",".");
+    $rural_f = number_format($rural,0,"",".");
+    $pop2010_f = number_format($pop2010,0,"",".");
+    
+    # Mostrar os estados que tem mais que 300 cidades e menos que 100 cidades mudando seu bg
+    if($quantidade_cidades > 300){
+        $corFundo = 'bg-primary-subtle';
+    }elseif($quantidade_cidades < 100){
+        $corFundo = 'bg-success-subtle';
     }else{
-
-        # Formatar com 0 casas decimais, nenhum separador decimal e o ponto como separador de milhar
-        $homens_f = number_format($homens,0,"",".");
-        $mulheres_f = number_format($mulheres,0,"",".");
-        $urbana_f = number_format($urbana,0,"",".");
-        $rural_f = number_format($rural,0,"",".");
-        $pop2010_f = number_format($pop2010,0,"",".");
-
-        # Mostrar os estados que tem mais que 300 cidades e menos que 100 cidades mudando seu bg
-        if($quantidade_cidades > 300){
-            $corFundo = 'bg-primary-subtle';
-        }elseif($quantidade_cidades < 100){
-            $corFundo = 'bg-success-subtle';
-        }else{
-            $corFundo = '';
-        }
-        
-        $tabela_linha="
+        $corFundo = '';
+    }
+    
+    $tabela_linha="
         <tr class = ''>
         <td class='text-center $corFundo'>$uf</td>
         <td class='text-center $corFundo'>$nomeEstado</td>
@@ -109,7 +105,7 @@ while(!feof($dados)){
             $nordeste.=$tabela_linha;
 
         }
-
+    }
     }
 }
 
---
+++ municipios.php
@@ -0,0 +1,96 @@
+<?php
+
+# Carregar o arquivo do template
+$template = file_get_contents("template.html");
+
+# Caminho do arquivo
+$arquivo = "arquivos_de_dados/municipios.csv";
+
+# Função do PHP para leitura de arquivos externos
+# r indica que o arquivoserá aberto apenas para leitura
+$dados = fopen($arquivo, "r");
+
+$tabela = "";
+$ufAnterior = "";
+
+while(!feof($dados)) {
+    
+    # Pegar a linha atual
+    $linha = fgets($dados);
+
+    # dividir a linha atual e gerar um array, usando o ";" como referência.
+    $coluna = explode(";", $linha);
+
+    # verificar se existem todos os itens no array
+    if(count($coluna) >= 10){
+        
+        $codigo = $coluna[0];
+        $uf = $coluna[1];
+        $municipio = $coluna[2];
+        $pop2000 = $coluna[3];
+        $homens = $coluna[4];
+        $mulheres = $coluna[5];
+        $urbana = $coluna[6];
+        $rural = $coluna[7];
+        $pop2010 = $coluna[8];
+        $pop2021 = $coluna[9];
+
+    
+        if($tabela==""){
+    
+            $tabela.="
+            <tr>
+            <th class='text-center'>$codigo</th>
+            <th class='text-center'>$uf</th>
+            <th class='text-center'>$municipio</th>
+            <th class='text-center'>$homens</th>
+            <th class='text-center'>$mulheres</th>
+            <th class='text-center'>$urbana</th>
+            <th class='text-center'>$rural</th>
+            <th class='text-center'>$pop2000</th>
+            <th class='text-center'>$pop2021</th>
+            </tr>
+            ";
+
+        }else{
+            
+            # Formatar com 0 casas decimais, nenhum separador decimal e o ponto como separador de milhar
+            $homens_f = number_format($homens,0,"",".");
+            $mulheres_f = number_format($mulheres,0,"",".");
+            $urbana_f = number_format($urbana,0,"",".");
+            $rural_f = number_format($rural,0,"",".");
+            $pop2000_f = number_format($pop2000,0,"",".");
+            $pop2021_f = number_format($pop2021,0,"",".");
+            
+            if($ufAnterior != $uf){
+                $tabela.= "<tr><td colspan='9'>Estado de $uf</td></tr>";
+            }
+            
+            $tabela.="
+            <tr class = ''>
+            <td class='text-center'>$codigo</td>
+            <td class='text-center'>$uf</td>
+            <td class='text-center'>$municipio</td>
+            <td class='text-center'>$homens_f</td>
+            <td class='text-center'>$mulheres_f</td>
+            <td class='text-center'>$urbana_f</td>
+            <td class='text-center'>$rural_f</td>
+            <td class='text-center'>$pop2000_f</td>
+            <td class='text-center'>$pop2021_f</td>
+            </tr>
+            ";
+            
+            $ufAnterior = $uf;
+        }
+}
+}
+
+
+
+
+$titulo = "Dados Populacionais dos Municipios Brasileiros";
+# Localizar a marcação "[[titulo]]" e substituir pelo conteúdo da variável $titulo
+$template = str_replace("[[titulo]]", $titulo, $template);
+$template = str_replace("[[tabela]]", $tabela, $template);
+
+echo $template;
