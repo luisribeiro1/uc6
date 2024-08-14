5ae3390d6ccfa0ecb04416b3a8f7771bcefd48bc
JOSE HENRIQUE DE BRITO DOS SANTOS <jose.hbsantos@senacsp.edu.br>
2024-08-13 22:18:05 -0300

    Aula do dia 13/08/2024 Atividade tabela dos estados formatado

--- estados2.php
+++ estados2.php
@@ -10,14 +10,7 @@ $arquivo = "arquivos_de_dados/estados.csv";
 # "r" indica que o arquivo será aberto apenas para leitura
 $dados = fopen($arquivo, "r");
 
-$tabela = "";
-
-$sudeste = "";
-$sul = "";
-$norte = "";
-$nordeste = "";
-$centroOeste = "";
-
+$conteudo = "";
 
 # Percoreer dados té que encontre o final do arquivo
 while(!feof($dados)){
@@ -45,85 +38,40 @@ while(!feof($dados)){
     $gentilico = $colunas[11];
     $area = $colunas[12];
     
-    
-    
-
-    if($tabela==""){
-        $tabela.="
-        <tr>
-            <th class='text-center'>$uf</th>
-            <th class='text-center'>$nomeEstado</th>
-            <th class='text-end'>$homens</th>
-            <th class='text-end'>$mulheres</th>
-            <th class='text-end'>$urbana</th>
-            <th class='text-end'>$rural</th>
-            <th class='text-end'>$pop2010</th>
-            <th class='text-center'>$capital</th>
-            <th class='text-center'>$quantidade_cidades</th>
-        </tr>    
-        ";
-    }else{
-
         # Formatar com 0 casas decimais, nenhum separador decimal e o ponto como separador de milhar
-        $homens_f = number_format($homens,0,"",".");
-        $mulheres_f = number_format($mulheres,0,"",".");
-        $urbana_f = number_format($urbana,0,"",".");
-        $rural_f = number_format($rural,0,"",".");
-        $pop2010_f = number_format($pop2010,0,"",".");
+        // $homens_f = number_format($homens,0,"",".");
+        // $mulheres_f = number_format($mulheres,0,"",".");
+        // $urbana_f = number_format($urbana,0,"",".");
+        // $rural_f = number_format($rural,0,"",".");
+        // $pop2010_f = number_format($pop2010,0,"",".");
 
-        # Dando cor para estados com mais de 300 cidades e estados com menos de 100 cidades
-        if($quantidade_cidades > 300){
-            $corFundo = 'bg-primary-subtle';
-        }elseif($quantidade_cidades < 100){
-            $corFundo = 'bg-success-subtle';
-        }else{
-            $corFundo = '';
-        }
+        $conteudo.="<div class='col-3 mb-4'>
+                <div class='card'>
+                    <div class='card-body text-center'>
+                        <span class='fw-bold'>$uf - $nomeEstado</span><br>
+                        <span class='fs-7'>
+                            <i class='bi bi-person-standing text-info'></i> $homens
+                            <i class='bi bi-person-standing-dress text-danger'></i> $mulheres
+                        </span>
+                        <br>
+                        <span class='fs-7'>
+                            <i class='bi bi-tree-fill text-success'></i> $rural
+                            <i class='bi bi-building-fill text-primary'></i> $urbana
+                        </span>
+                    </div>
+                </div>
+            </div>";
 
-        $tabela_linha="
-        <tr>
-        <td class='text-center $corFundo'>$uf</td>
-        <td class='text-center $corFundo'>$nomeEstado</td>
-        <td class='text-end $corFundo'>$homens_f</td>
-        <td class='text-end $corFundo'>$mulheres_f</td>
-        <td class='text-end $corFundo'>$urbana_f</td>
-        <td class='text-end $corFundo'>$rural_f</td>
-        <td class='text-end $corFundo'>$pop2010_f</td>
-        <td class='text-center $corFundo'>$capital</td>
-        <td class='text-center $corFundo'>$quantidade_cidades</td>
-        </tr>    
-        ";
         
-        
-        # Separando por região
-    if($uf == 'SP' or $uf== 'RJ' or $uf== 'MG' or $uf== 'ES'){
-        $sudeste.=$tabela_linha;
-    }if($uf == 'PR' or $uf== 'SC' or $uf== 'RS'){
-        $sul.=$tabela_linha;
-    }if($uf == 'MS' or $uf== 'MT' or $uf== 'GO' or $uf== 'DF'){
-        $centroOeste.=$tabela_linha;
-    }if($uf == 'AC' or $uf== 'AM' or $uf== 'RR' or $uf== 'RO' or $uf== 'AP' or $uf== 'PA' or $uf== 'TO'){
-        $norte.=$tabela_linha;
-    }if($uf == 'MA' or $uf== 'PI' or $uf== 'CE' or $uf== 'RN' or $uf== 'PB' or $uf== 'PE' or $uf== 'AL' or $uf== 'SE' or $uf== 'BA'){
-        $nordeste.=$tabela_linha;
-    }
- 
-    }
     }
 
 }
-
-$tabela.= "<tr><td colspan='9'><b>Região Sudeste</b></td></tr>$sudeste";
-$tabela.= "<tr><td colspan='9'><b>Região Sul<b/></td></tr>$sul";
-$tabela.= "<tr><td colspan='9'><b>Região Centro-Oeste</b></td></tr>$centroOeste";
-$tabela.= "<tr><td colspan='9'><b>Região Norte</b></td></tr>$norte";
-$tabela.= "<tr><td colspan='9'><b>Região Nordeste</b></td></tr>$nordeste";
 
 $titulo = "Dados populacionais dos estados brasileiros";
 
 # Localizar a marcação [[titulo]] e substituir pelo conteúdo da variável titulo
 $template = str_replace("[[titulo]]",$titulo, $template);
-$template = str_replace("[[tabela]]",$tabela, $template);
+$template = str_replace("[[conteudo]]",$conteudo, $template);
 
 echo $template;
 
