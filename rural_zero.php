721cbbde419530446af9418cbe487cdcaf759a36
LETICIA MARIA SILVA RAMOS DE OLIVEIRA <leticia.msroliveira@senacsp.edu.br>
2024-08-22 22:11:46 -0300

    Aula 22/08/2024: UC6|Array Associativo|Rural Zero

---
+++ rural_zero.php
@@ -0,0 +1,39 @@
+<?php
+
+$rural_zero = [];
+
+$arquivo = "arquivos_de_dados/municipios.csv";
+
+$dados = fopen($arquivo, "r");
+
+while(!feof($dados)){
+
+    $linha = fgets ($dados);
+
+    $colunas = explode (";",$linha);
+
+    if(count($colunas) >= 10){
+
+        $codigo = $colunas[0];
+        $uf = $colunas[1];
+        $municipios = $colunas[2];
+        $pop2000 = $colunas[3];
+        $homens = $colunas[4];
+        $mulheres = $colunas[5];
+        $urbana = $colunas[6];
+        $rural = $colunas[7];
+        $pop2010 = $colunas[8];
+        $pop2021 = $colunas[9];
+
+        if($rural == 0){
+            $rural_zero[$municipios] = "$uf - $rural";
+        }
+        
+    }
+    
+    ksort($rural_zero);
+}
+
+foreach($rural_zero as $municipios => $uf){
+    echo"• $uf: $municipios <br>";
+}
