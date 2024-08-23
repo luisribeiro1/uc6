f8a3548914d0addb36386f246cf0b4f71079a52b
KAINA DANIEL DA SILVA CORREA <kaina.dscorrea@senacsp.edu.br>
2024-08-22 22:11:23 -0300

    Aula 22/08/2024: Atividade 11 e 12: Array_associativo.php rural_zero.php UC6

---
+++ rural_zero.php
@@ -0,0 +1,39 @@
+<?php
+
+$rural_zero = [];
+
+$arquivo = "arquivos_de_dados/municipios.csv";
+
+$dados = fopen($arquivo,"r");
+
+while(!feof($dados)){
+    $linha = fgets($dados);
+
+    $colunas = explode(";",$linha);
+
+    if(count($colunas) >= 10){
+        
+        $codigo = $colunas[0];
+        $uf = $colunas[1];
+        $cidade = $colunas[2];
+        $pop2000 = $colunas[3];
+        $homens = $colunas[4];
+        $mulheres = $colunas[5];
+        $urbana = $colunas[6];
+        $rural = $colunas[7];
+        $pop2010 = $colunas[8];
+        $pop2021 = $colunas[9];
+        
+        if($rural == 0){
+            $rural_zero[$cidade] = "$uf - $rural";
+        }
+        
+    }
+
+    ksort($rural_zero);
+
+}
+
+        foreach($rural_zero as $chave => $valor){
+            echo "• $valor: $chave <br>";
+        }
