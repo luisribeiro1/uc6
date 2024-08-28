6c9629d6bcfaa6fd12bf0f619b1e816cea1d81cb
JOSE HENRIQUE DE BRITO DOS SANTOS <jose.hbsantos@senacsp.edu.br>
2024-08-27 22:14:45 -0300

    Aula do dia 27/08/2024 Carrinho

---
+++ class_carrinho.php
@@ -0,0 +1,28 @@
+<?php
+
+# Diz ao PHP que iremos manipular sessões nesta página
+session_start();
+
+class Carrinho {
+
+    # Método construtor
+    public function __construct() {
+
+        # Se a sessão não existir
+        if (!isset($_SESSION['carrinho'])) {
+            $_SESSION['carrinho'] = [];     // Cria a sessão com um array vazio
+        }    
+    }
+
+    public function adicionar($item) {  // Recebe o array como parâmetro
+        $_SESSION['carrinho'][] = $item;
+    }
+
+    public function remover($indice) {
+
+    }
+
+    public function listar() {
+        return $_SESSION['carrinho'];
+    }
+}
