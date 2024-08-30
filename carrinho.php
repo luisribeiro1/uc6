5de3f0c9261412fe89e980b82df5a6b1eacd9bf4
ERICK SILVA SOUZA DO PRADO <erick.ssprado@senacsp.edu.br>
2024-08-29 22:21:26 -0300

    Atividade Desconto  29/08

--- carrinho.php
+++ carrinho.php
@@ -14,13 +14,26 @@ $carrinho = new Carrinho();
 
 # Verificar se o formulário foi submetido
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $item = [
-        'nomeProduto' => $_POST['nomeProduto'],
-        'quantidade' => $_POST['quantidade'],
-        'preco' => $_POST['preco']
-        
-    ];
-    $carrinho->adicionar($item);
+    
+    
+    if(isset($_POST["adicionar"])){    // Botão adicionar clicado
+        $item = [
+            'nomeProduto' => $_POST['nomeProduto'],
+            'quantidade' => $_POST['quantidade'],
+            'preco' => $_POST['preco']
+            
+        ];
+        $carrinho->adicionar($item);
+    }
+    elseif(isset($_POST["remover"])){  // qualquer botão remover clicado
+        $indice = $_POST["indice"];
+        $carrinho->remover($indice);
+    }
+    
+    
+    if(isset($_POST["cupomDesconto"])){    // Botão adicionar clicado
+       $carrinho->aplicarDesconto($_POST['desconto']);
+    }
 }
 
 # preencher a variável $arrray_itens com o array da sessão que o método listar devolverá
@@ -28,7 +41,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 $array_itens = $carrinho->listar();
 $carrinho_vazio = "";
 $carrinho_html ="";
-
+$total = 0;
 if(empty($array_itens)){
     $carrinho_vazio = "<p class=' alert alert-info '>Nenhum item no carrinho</p> ";
 }else{ 
@@ -37,19 +50,65 @@ if(empty($array_itens)){
         $nome = $unidade['nomeProduto'];
         $quantidade = $unidade['quantidade'];
         $preco = $unidade['preco'];
+        $subtotal = $quantidade * $preco;
+        
+        $total += $subtotal;
+
+
+        # Aplicar Formatação decimal
+        $preco = number_format($preco,2,",",".");
+        $subtotal = number_format($subtotal,2,",",".");
+        
+        
+        
         $carrinho_html .= "
-            <tr>
-                <td>$nome</td>
-                <td>$quantidade</td>
-                <td>$preco</td>
-                <td></td>
-                <td></td>
-            </tr>
+        <tr>
+        <td>$nome</td>
+        <td>$quantidade</td>
+        <td class='text-end'>$preco</td>
+        <td class='text-end'>$subtotal</td>
+        <td class='text-center'>
+        <form method='post'>
+        <input type='hidden' name='indice' value='$indice'> 
+        <button type ='submit' name='remover' class='btn btn-danger '>Remover</button>
+        </form>
+        </td>
+        </tr>
         ";
     }   
+    #
+    $desconto = $carrinho->pegarDesconto();
+    $descontoValor = $total * $desconto / 100;
+    $total -= $descontoValor;
+
+
+
+
+    
+    $total = number_format($total,2,",",".");
+    
+    $carrinho_html .= "
+    <tr>
+        <td></td>
+        <td></td>
+        <td class='text-end fw-bold'>Desconto:</td>
+        <td class='text-end fw-bold'>$descontoValor</td>
+        <td></td>
+    </tr>
+    <tr>
+        <td></td>
+        <td></td>
+        <td class='text-end fw-bold'>Total:</td>
+        <td class='text-end fw-bold'>$total</td>
+        <td></td>
+    </tr>
+    ";
+
+
 }
 
 
+# incluir o total do carrinho
 
 
 # importar o tamplate 
--- class_carrinho.php
+++ class_carrinho.php
@@ -10,17 +10,27 @@ class Carrinho {
 
         if(!isset($_SESSION['carrinho'])) {                     //mostra se existe algo em PHP
 
-            $_SESSION['carrinho'] = [];      // Cria a sessão com um array vazio
+            $_SESSION['carrinho'] = [];  
+            $_SESSION['desconto'] = 0;    // Cria a sessão com um array vazio
         }
+       
     }
     public function adicionar($item){ // Recebe o array como parâmetro
         $_SESSION['carrinho'][] = $item;
     }
     public function remover($indice){
-        
+        unset($_SESSION['carrinho'][$indice]);
     }
     public function listar(){
         return $_SESSION['carrinho'];
+    }
+
+
+    public function aplicarDesconto($desconto){
+        $_SESSION['desconto'] = $desconto;
+    }
+    public function pegarDesconto(){
+        return $_SESSION['desconto'];
     }
 
     
