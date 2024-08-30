2edc4b7811161be68bbc8be7564ed0d59ff5e0fb
FERNANDO MARTINS CASIMIRO <fernando.mcasimiro@senacsp.edu.br>
2024-08-29 22:09:18 -0300

    Atividade no carrinho.php - 29/08/2024

--- carrinho.php
+++ carrinho.php
@@ -14,18 +14,28 @@ $carrinho = new Carrinho();
 
 # Verificar se o formulário foi submetido.
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $item = [
-        'nomeProduto' => $_POST['nomeProduto'],
-        'quantidade' => $_POST['quantidade'],
-        'preco' => $_POST['preco']
-    ];
-    $carrinho->adicionar($item);
+    if (isset($_POST["adicionar"])){
+        $item = [
+            'nomeProduto' => $_POST['nomeProduto'],
+            'quantidade' => $_POST['quantidade'],
+            'preco' => $_POST['preco']
+        ];
+        $carrinho->adicionar($item);
+    }elseif(isset($_POST["remover"])){
+        $indice = $_POST["indice"];
+        $carrinho->remover($indice);
+    }elseif (isset($_POST['cupomDesconto'])) {
+        $desconto = $_POST['desconto'];
+        $carrinho->aplicarDesconto($desconto);
+    }
 }
 
 $array_itens = $carrinho->listar();
 
 $carrinho_vazio = "";
 $carrinho_html = "";
+$total = 0;
+$desconto = 0;
 
 if (empty($array_itens)) {
     $carrinho_vazio = "<p class='alert alert-info'>Nenhum item no carrinho</p>";
@@ -34,18 +44,47 @@ if (empty($array_itens)) {
         $nome = $unidade['nomeProduto'];
         $quantidade = $unidade['quantidade'];
         $preco = $unidade['preco'];
+        $subtotal = $quantidade * $preco;
+        $total += $subtotal;
+
+        # Aplicar formatação decimal.
+        $preco = number_format($preco, 2,",","."); 
+        $subtotal = number_format($subtotal, 2,",","."); 
 
         $carrinho_html .= "
         <tr>
             <td>$nome</td>
             <td>$quantidade</td>
-            <td>$preco</td>
-            <td></td>
-            <td></td>
+            <td class='text-end'>$preco</td>
+            <td class='text-end'>$subtotal</td>
+            <td class='text-center'>
+                <form method='post'>
+                    <input type='hidden' name='indice' value='$indice'>
+                    <button type='submit' name='remover' class='btn btn-danger'>Remover</button>
+                </form>
+            </td>
         </tr>
         ";
     }
 }
+
+$desconto = $carrinho->pegarDesconto();
+$descontoValor = ($total * $desconto) / 100;
+$total -= $descontoValor;
+
+# Incluir o total no carrinho.
+$total = number_format($total, 2,",",".");
+$descontoValor = number_format($descontoValor, 2,",",".");
+$carrinho_html .= "
+    <tr>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td class='text-end'><strong>Desconto: $descontoValor</strong></td>
+        <td class='text-end'><strong>Total: $total</strong></td>
+        <td></td>
+    </tr>
+";
 
 # Importar o template.
 $html = file_get_contents("template_carrinho.html");
--- class_carrinho.php
+++ class_carrinho.php
@@ -9,8 +9,9 @@ class Carrinho {
         # Se a sessão não existir.
         if (!isset($_SESSION['carrinho'])) {
             $_SESSION['carrinho'] = [];  // Cria a sessão com um array vazio.
-        }
-        
+        }elseif (!isset($_SESSION['desconto'])) {
+            $_SESSION['desconto'] = 0;
+        }    
     }
 
     public function adicionar($item) {
@@ -18,10 +19,20 @@ class Carrinho {
     }
 
     public function remover($indice){
-        
+        unset($_SESSION['carrinho'][$indice]);
     }
 
-    public function listar(){
+    public function listar() {
         return $_SESSION['carrinho'];
     }
+
+
+    public function aplicarDesconto($percentual) {
+        $_SESSION['desconto'] = $percentual;
+    }
+
+    public function pegarDesconto() {
+        return $_SESSION['desconto'];
+    }
+
 }
