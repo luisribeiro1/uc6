40688356ae16edadad64f73d7a37a0346c03d6c0
JOSE HENRIQUE DE BRITO DOS SANTOS <jose.hbsantos@senacsp.edu.br>
2024-09-03 19:25:35 -0300

    Atividade desconto terminado

--- carrinho.php
+++ carrinho.php
@@ -2,8 +2,8 @@
 
 require_once 'class_carrinho.php';
 
-# Criar a lista de produtos
-$produtos = ["Arroz","Feijão","Batata","Azeite","Miojo","Bolacha","Queijo","Banana"];
+# Criar a linha de produtos
+$produtos = ["Arroz", "Feijão", "Batata", "Azeite", "Miojo", "Bolacha", "Queijo", "Banana"];
 
 $listaProdutos = "";
 foreach($produtos as $produto){
@@ -14,30 +14,38 @@ foreach($produtos as $produto){
 $carrinho = new Carrinho();
 
 # Verificar se o formulário foi submetido
-if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-
-    if (isset($_POST["adicionar"])){  // Botão adicionar clicado
-    $item = [
+if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if(isset($_POST["adicionar"])){        // botão adicionar clicado
+    $item =[
         'nomeProduto' => $_POST['nomeProduto'],
         'quantidade' => $_POST['quantidade'],
-        'preco' => $_POST['preco'],     
+        'preco' => $_POST['preco']
     ];
     $carrinho->adicionar($item);
-}    
-elseif(isset($_POST["remover"])){  // Qualquer botão remover clicado
+}
+elseif(isset($_POST["remover"])){      // qualquer botão remover clicado
     $indice = $_POST["indice"];
     $carrinho->remover($indice);
 }
+
+elseif(isset($_POST["cupomDesconto"])){ // Se o cupom de desconto foi enviado
+    $desconto = $_POST['desconto'];
+    if ($desconto !== '') {
+        $carrinho->aplicarDesconto($desconto);
+    }
+
 }
 
-# Preencher a variável $array_itens com o array da sessão que o método listar devolverá
+}
+
+# preencher a variável $array_itens com o array da sessão que o método listar devolverá
 $array_itens = $carrinho->listar();
 
 $carrinho_vazio = "";
 $carrinho_html = "";
 $total = 0;
 
-if (empty($array_itens)){
+if (empty ($array_itens)) {
     $carrinho_vazio = "<p class='alert alert-info'>Nenhum item no carrinho</p>";
 } else {
     foreach($array_itens as $indice => $unidade){
@@ -46,48 +54,52 @@ if (empty($array_itens)){
         $quantidade = $unidade['quantidade'];
         $preco = $unidade['preco'];
         $subtotal = $quantidade * $preco;
-        $total +=$subtotal;     // $total = $total + $subtotal;
+        $total +=$subtotal;
 
         # Aplicar formatação decimal
-        $preco = number_format($preco, 2, ",", ".");
-        $subtotal = number_format($subtotal, 2, ",", ".");
-        
+        $preco = number_format($preco,2,",",".");
+        $subtotal = number_format($subtotal,2,",",".");
+
         $carrinho_html .= "
         <tr>
-        <td>$nome</td>
-        <td>$quantidade</td>
-        <td class= 'text-end'>$preco</td>
-        <td class= 'text-end'>$subtotal</td>
-        <td class='text-center'>
-        <form method='post'>
-        <input type='hidden' name='indice' value='$indice'>
-        <button type='submit' name='remover' class='btn btn-danger'>Remover</button>
-        </form>
-        </td>
+            <td>$nome</td>
+            <td>$quantidade</td>
+            <td class = 'text-end'>$preco</td>
+            <td class = 'text-end'>$subtotal</td>
+            <td class = 'text-center'>
+                <form method='post'>
+                <input type = 'hidden' name = 'indice' value = '$indice'>
+                    <button type ='submit' name ='remover' class = 'btn btn-danger'>Remover</button>
+                </form>
+            </td>
         </tr>
         ";
     }
 }
 
+# Calcular o valor do desconto
+$desconto = $carrinho->pegarDesconto();
+$descontoValor = ($total * $desconto) / 100;
+$total -= $descontoValor;
+$descontoValor = number_format($descontoValor, 2, ",", ".");
+
 # Incluir o total do carrinho
-$total = number_format($total, 2, ",", ".");
+
+$total = number_format($total,2,",",".");
 
 $carrinho_html .= "
-
 <tr>
-<td></td>
     <td></td>
-    <td class= 'text-end fw-bold'>Total</td>
-    <td class= 'text-end fw-bold'>$total</td>
-    <td </td>
+    <td></td>
+    <td class = 'text-end fw-bold'>Total</td>
+    <td class = 'text-end fw-bold'>$total</td>
+    <td></td>
 </tr>
 ";
 
 # Importar o template
 $html = file_get_contents("template_carrinho.html");
-$html = str_replace("[[listaProdutos]]",$listaProdutos,$html);
-$html = str_replace("[[carrinho_vazio]]",$carrinho_vazio,$html);
-$html = str_replace("[[carrinho]]",$carrinho_html,$html);
-
-
+$html = str_replace("[[listaProdutos]]", $listaProdutos,$html);
+$html = str_replace("[[carrinho_vazio]]", $carrinho_vazio,$html);
+$html = str_replace("[[carrinho]]", $carrinho_html,$html);
 echo $html;
